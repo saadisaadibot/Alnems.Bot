6481f67a0edfe61b42fc885015dbce769fc50ee4
saadisaadibot <alhossiniabdulhalim3@gmail.com>
2025-08-02 02:23:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def buy(symbol):
                 "trail_percent": 0.5,
                 "max_profit": 0
             }))
-            send_message(f"✅ شراء {symbol} بسعر {price:.4f}")
+            send_message(f"✅ شراء {symbol} بسعر {price:.8f}")
             return price, amount
 
         except Exception as e:
